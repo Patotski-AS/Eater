205d9d65184f8937130eaaab0f8ada10f92d5ea8
pototskiy_as <a.pototskiy@horizont-rnd.by>
2023-06-07 14:11:09 +0300

    feat: added pojo models

---
+++ data/src/main/java/pas/eater/data/data_sourse/api/pojo/Dishe.kt
@@ -0,0 +1,18 @@
+package pas.eater.data.data_sourse.api.pojo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DisheResponse(
+    @SerializedName("dishes")
+    val dishes: List<Dishe>
+)
+
+data class Dishe(
+    @SerializedName("description") val description: String,
+    @SerializedName("id") val id: Int,
+    @SerializedName("image_url") val imageUrl: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("price") val price: Int,
+    @SerializedName("tegs") val tegs: List<String>,
+    @SerializedName("weight") val weight: Int)
