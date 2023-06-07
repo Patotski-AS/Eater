067fc45aa7fb22d00880145160422de899f4fa27
pototskiy_as <a.pototskiy@horizont-rnd.by>
2023-06-07 14:14:27 +0300

    feat: added RemoteException helper

---
+++ data/src/main/java/pas/eater/data/data_sourse/api/helper/RemoteException.kt
@@ -0,0 +1,10 @@
+package pas.eater.data.data_sourse.api.helper
+
+import okio.IOException
+
+sealed class RemoteException(message: String): IOException(message) {
+    class ClientError(message: String): RemoteException(message)
+    class ServerError(message: String): RemoteException(message)
+    class NoNetworkError(message: String): RemoteException(message)
+    class GenericError(message: String): RemoteException(message)
+}
